cceb5f01541ff0fb6a8117e3c80001e11876404b
viratech07 <virat.inclined@gmail.com>
2026-04-18 03:36:46 +0530

    Update profile to B.Pharm student; update contact email and location

--- lib/download-links.ts
+++ lib/download-links.ts
@@ -1,3 +1,3 @@
 /** Public Android APK download URL. Update when you publish a new build. */
 export const ANDROID_APK_URL =
-  "https://drive.google.com/file/d/1oid8Go6it7ZFdy_bjrKNAX_u9Mf0i_Kv/view?usp=sharing";
+  "https://drive.google.com/file/d/1HDb2q5kOd19ieQhB0ySWMWlxapYa1ese/view?usp=sharing";
